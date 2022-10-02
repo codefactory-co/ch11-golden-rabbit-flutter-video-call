c73e2ad273ef190c1b3968dc0f4fc95d1ffb112b
JC <withj1123@gmail.com>
2022-10-02 17:41:11 +0900

    done

---
+++ lib/const/agora.dart
@@ -0,0 +1,5 @@
+const APP_ID = '<여기에 API키를 입력해주세요>';
+
+const CHANNEL_NAME = 'testchannel';
+
+const TEMP_TOKEN = '007eJxTYHjTfnyHd063ge82h9+hL6VrMnkyJrsEzNA/bRmWaczo1qHAkGaalJZmbmRkbm5papJkYZGUlmpoYmpokWhplmhhbGSk7WmWvHqWebJhvQ0jIwMEgvjcDCWpxSXJGYl5eak5DAwAwXghHg==';
